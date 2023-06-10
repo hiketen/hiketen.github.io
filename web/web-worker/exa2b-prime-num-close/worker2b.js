e628ac37ff108a3a69f804f64e28a49fb5823ac4
hiketen.skd16 <hiketen.skd16@komidi.com>
2023-06-10 14:57:15 -0700

    .

---
+++ web/web-worker/exa2b-prime-num-close/worker2b.js
@@ -0,0 +1,21 @@
+postMessage("Hey, in 3s, I'll start to compute prime numbers...");
+
+setTimeout(function() {
+    // The setTimeout is just useful for displaying the message in line 1 for 3 seconds and
+    // make it visible
+    var n = 1;
+    search: while (true) {
+	n += 1;
+	for (var i = 2; i <= Math.sqrt(n); i += 1)
+	    if (n % i == 0)
+		continue search;
+	    // found a prime!
+	postMessage(n);
+        if (n > 100000) {
+	    // close is not working - to be debugged
+	    // postMessage("Terminating myself");
+	    // self.close();  
+	    break;
+	}
+    }}, 3000);
+
